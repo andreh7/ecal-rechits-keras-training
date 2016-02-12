b6c5666dc5bbfb17d3f21e50419313fb5f3cf10e
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2016-02-12 22:47:31 +0100

    added missing imports in model file

--- model01.py
+++ model01.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+
 
 #----------------------------------------------------------------------
 
