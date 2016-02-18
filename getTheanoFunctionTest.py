029e458427d73333bda735e21029701e29a1a0a7
andreh <andreh@localhost>
2016-02-18 21:59:06 +0100

    now producing a graphviz graph of the model if pydot and graphviz available

--- getTheanoFunctionTest.py
+++ getTheanoFunctionTest.py
@@ -2,7 +2,9 @@
 
 # trying to get the theano function out of the keras model
 
-execfile("model01.py")
+modelFname = "model01.py"
+
+execfile(modelFname)
 
 model = makeModel((1, 7, 23))
 
@@ -23,4 +25,20 @@ debugprint(func,
 # see e.g. https://github.com/Theano/Theano/blob/master/theano/printing.py#L44
 #----------
 
+# check if we can produce a graphviz graph
+havePydot = False
+try:
+    import pydot
+    if pydot.find_graphviz():
+        havePydot = True
+except ImportError:
+    pass
+
+if havePydot:
+    print "generating a graphviz file"
+    theano.printing.pydotprint(func,
+                               outfile = modelFname + ".svg",
+                               format = "svg",
+                               # var_with_name_simple = True,
+        )
 
